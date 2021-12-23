52e714d6c7614213a672587fb2f8e310b9bf882f
Jahnab Dutta <76475829+JahnabDutta@users.noreply.github.com>
2021-12-23 10:43:10 +0530

    initial commit

---
+++ frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class CreateRoomPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() { 
+        return (<p>This is our Create room Page</p>);
+    }
+}
+ 
+export default CreateRoomPage;
